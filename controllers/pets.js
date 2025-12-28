2c6f87b57eb7dadcc4893648c2a780ba6c5f4a24
fadhelhaji <fadhelmh@hotmail.com>
2025-12-28 15:55:01 +0300

    added a post request that creates new post, fully functional and ready to push into production

--- controllers/pets.js
+++ controllers/pets.js
@@ -3,9 +3,16 @@ const express = require('express')
 const router = express.Router()
 
 router.post('/', async (req, res)=>{
-    res.json(req.body)
+    try {
+        const createPet = await Pet.create(req.body)
+        res.status(201).json({
+            message: "Pet succesfully created!",
+            pet: createPet
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: 'Failed to create pet:('})
+    }
 })
-
-
 
 module.exports = router
